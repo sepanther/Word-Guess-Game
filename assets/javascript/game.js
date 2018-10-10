5a453e3493a630b51943260c7e6c0539de3f90aa
sepanther <sepanta.mohseni@gmail.com>
2018-10-09 22:01:44 -0400

    added some things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // VARIABLES
 
 //The array of words that will be chosen at random
-var words = ["test"]
+var words = ["test", "hello", "world"]
 
 //Start with score of 0
 var score = 0
@@ -9,8 +9,13 @@ var score = 0
 //Start with empty secretWord and secretDisplay variables
 var secretWord = ""
 var secretDisplay = ""
+var workshop = ""
 
-//Start with 
+//Start with empty lettersGuessed and wrongLetters string/array
+var lettersGuessed = ""
+var wrongLetters = []
+var numGuesses = 8
+var numWins = 0
 
 //FUNCTIONS
 
@@ -26,6 +31,19 @@ function createDisplay() {
     }
     }
 
+//Function to update the secretWord
+function updateWord() {
+    var workshop = ""
+    for (i = 0; i < secretWord.length; i++) {
+        if (lettersGuessed.includes(secretWord[i])) {
+            workshop = workshop + secretWord[i]
+        }
+        else {
+            workshop = workshop + "_"
+        }
+    }
+}
+
 //Function to display word in DOM
 function updateDisplay() {
     document.getElementById("display").innerHTML = secretDisplay;
@@ -33,12 +51,31 @@ function updateDisplay() {
 
 //Function to update number of guesses
 function updateGuesses() {
-    document.querySelector("guesses").innerHTML = "Guesses: " + guess
+    document.getElementById("guesses").innerHTML = "Guesses: " + guess
 }
 
-//Function to update letters guessed
-function updateLettersGuessed() {
+//Function to check if word is guessed
+function isWordGuessed() {
+    for (i = 0; i < secretWord.length; i++) {
+        if ((lettersGuessed.includes(secretWord[i])) === false) {
+            return false;
+        }
+    }
+    numWins++
+    console.log(numWins);
+    document.getElementById("wins").innerHTML = numWins;
+    return true;
+}
 
+//Function to start a new game
+function newGame() {
+    chooseWord();
+    secretDisplay = "";
+    workshop = "";
+    lettersGuessed = "";
+    numGuesses = 8;
+    createDisplay();
+    updateDisplay();
 }
 
 //MAIN PROCESS
@@ -47,4 +84,39 @@ chooseWord();
 createDisplay();
 updateDisplay();
 
+document.onkeyup = function(event) {
+    if (lettersGuessed.includes(event.key) === false) {
+        guess = event.key;
+        lettersGuessed = lettersGuessed + guess
+        console.log(guess);
+        console.log(lettersGuessed);
+        if (secretWord.includes(guess) === false) {
+            numGuesses -= 1;
+            console.log(numGuesses)
+            if (numGuesses === 0) {
+                document.getElementById("guesses").textContent = "You're out of guesses haha you suck";
+            }
+        }
+        var workshop = ""
+        for (i = 0; i < secretWord.length; i++) {
+            if (lettersGuessed.includes(secretWord[i])) {
+            workshop = workshop + secretWord[i]
+            }
+        
+            else {
+            workshop = workshop + "_"
+            }
+        }
+
+
+    
+    secretDisplay = workshop
+    // workshop = ""
+    updateDisplay()
+
+    if (isWordGuessed()) {
+        newGame();
+    }
+}
+}
 
