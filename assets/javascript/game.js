755fda236974f2abfe02e363ddfdc31f5d03fda1
sepanther <sepanta.mohseni@gmail.com>
2018-10-10 20:05:32 -0400

    moving on to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,11 +47,12 @@ function updateWord() {
 //Function to display word in DOM
 function updateDisplay() {
     document.getElementById("display").innerHTML = secretDisplay;
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed
 }
 
 //Function to update number of guesses
 function updateGuesses() {
-    document.getElementById("guesses").innerHTML = "Guesses: " + guess
+    document.getElementById("guesses").innerHTML = "Guesses: " + numGuesses
 }
 
 //Function to check if word is guessed
@@ -63,9 +64,18 @@ function isWordGuessed() {
     }
     numWins++
     console.log(numWins);
-    document.getElementById("wins").innerHTML = numWins;
+    document.getElementById("wins").innerHTML = "Wins: " + numWins;
     return true;
 }
+
+//Function to wait ms milliseconds
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
 
 //Function to start a new game
 function newGame() {
@@ -74,6 +84,7 @@ function newGame() {
     workshop = "";
     lettersGuessed = "";
     numGuesses = 8;
+    updateGuesses();
     createDisplay();
     updateDisplay();
 }
@@ -84,20 +95,34 @@ chooseWord();
 createDisplay();
 updateDisplay();
 
+//When user presses key
 document.onkeyup = function(event) {
+
+//If this is a letter that has not been previously guessed
     if (lettersGuessed.includes(event.key) === false) {
         guess = event.key;
+
+//Add the letter to list of guessed letters
         lettersGuessed = lettersGuessed + guess
-        console.log(guess);
-        console.log(lettersGuessed);
+
+//If the letter is not in the word
         if (secretWord.includes(guess) === false) {
             numGuesses -= 1;
-            console.log(numGuesses)
+            updateGuesses();
+
+//If the user is out of guesses
+//Also, this clause is not currently being tested because a new game starts immediately when numGuesses = 0
             if (numGuesses === 0) {
-                document.getElementById("guesses").textContent = "You're out of guesses haha you suck";
+                var guessDOM = document.getElementById("guesses");
+                var youLose = document.createElement("div");
+                youLose.textContent = "You lose haha you suck";
+                guessDOM.appendChild(youLose);
             }
         }
+        
         var workshop = ""
+
+//Function to update which "secret word" letters are displayed in DOM        
         for (i = 0; i < secretWord.length; i++) {
             if (lettersGuessed.includes(secretWord[i])) {
             workshop = workshop + secretWord[i]
@@ -107,14 +132,15 @@ document.onkeyup = function(event) {
             workshop = workshop + "_"
             }
         }
-
-
-    
+//Convert "workshop" to secretDisplay which is the variable that will be shown
     secretDisplay = workshop
-    // workshop = ""
     updateDisplay()
 
-    if (isWordGuessed()) {
+//This is causing problems right now. Basically when I try to use the wait function, it doesn't complete the last "updateGuess" and say "haha you suck"... will come back to this later
+    // if (isWordGuessed() || document.getElementById("guesses").innerHTML === "Guesses: 0<div>You lose haha you suck</div>") {
+    //     wait(3000);
+
+    if (isWordGuessed() || numGuesses === 0) {
         newGame();
     }
 }
